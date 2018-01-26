75981439b868cad7912beab917850d6b67b6e9dc
Toto Lin <csl@lunlimited.net>
2018-01-26 23:08:57 +0900

    Lint and version bump

--- src/components/toolbar-button/btn-info.js
+++ src/components/toolbar-button/btn-info.js
@@ -3,8 +3,8 @@ export default {
     icon: 'fa-info-circle',
     title: '关于',
     action: {
-        insert: function () {
-            window.location.href="https://github.com/luogu-dev/markdown-palettes";
+        insert () {
+            window.location.href = 'https://github.com/luogu-dev/markdown-palettes'
         }
     }
 }
