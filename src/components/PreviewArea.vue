738dd684dda1ca5dce43d7e60d7586a8abc6bca4
Toto Lin <csl@lunlimited.net>
2018-06-30 19:58:35 +0900

    Fix preview styles and switch to yarn. Release 0.3.0.

--- src/components/PreviewArea.vue
+++ src/components/PreviewArea.vue
@@ -6,6 +6,7 @@
 
 <style>
     #mp-preview-content {
+        color: #333;
         padding: 10px;
         padding-left: 20px;
         padding-right: 20px;
@@ -37,6 +38,43 @@
     code.mp-code {
         font-family: Monaco, Menlo, Consolas, "Courier New", monospace;
         font-size: 15px;
+    }
+
+    #mp-preview-content img {
+        max-width: 100%;
+    }
+
+    #mp-preview-content p {
+        margin: 1rem 0;
+    }
+
+    #mp-preview-content h1, #mp-preview-content h2, #mp-preview-content h3,
+    #mp-preview-content h4, #mp-preview-content h5, #mp-preview-content h6 {
+        margin: .5rem 0;
+    }
+
+    #mp-preview-content h1, #mp-preview-content h2 {
+        padding-bottom: .2em;
+        border-bottom: solid 1px #eee;
+    }
+
+    #mp-preview-content ul, #mp-preview-content ol {
+        padding-left: 1.5em;
+    }
+
+    #mp-preview-content ul {
+        list-style: outside disc;
+    }
+
+    #mp-preview-content ol {
+        list-style: outside decimal;
+    }
+
+    #mp-preview-content hr {
+        margin: 1em 0;
+        height: 0;
+        border: none;
+        border-bottom: solid 1px #eee;
     }
 </style>
 
