88b01414329a7c72676fa384ee0b67879c961006
TitanSnow <sweeto@live.cn>
2018-07-03 03:34:56 +0800

    remove MarkdownParser.js, do not create converter each time

--- src/components/plugin/MarkdownParser.js
+++
@@ -1,9 +0,0 @@
-import MarkdownIt from 'markdown-it'
-
-export function MarkdownParser (code, parsers) {
-    let converter = MarkdownIt()
-    parsers.forEach(parser => {
-        converter = converter.use(parser)
-    })
-    return converter.render(code)
-}
