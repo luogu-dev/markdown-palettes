7bc7c83f6fcf77f98a6a05b75bd3eb157161e61f
TitanSnow <sweeto@live.cn>
2018-07-03 09:38:41 +0800

    Revert "support extra arguments passing to .use()"
    This reverts commit b229d4b513a358bee7bbdbf05d4cf1e378675cca.

--- src/components/ContentParserFactory.js
+++ src/components/ContentParserFactory.js
@@ -3,11 +3,7 @@ import MarkdownIt from 'markdown-it'
 function contentParserFactory (parsers) {
     let converter = MarkdownIt()
     parsers.forEach(parser => {
-        if(Array.isArray(parser))
-            // if parser is Array, it contains extra arguments to pass to .use()
-            converter = converter.use(...parser)
-        else
-            converter = converter.use(parser)
+        converter = converter.use(parser)
     })
     return content => converter.render(content)
 }
