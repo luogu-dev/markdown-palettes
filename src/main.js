839742e217d97fa5b1bdca70dfa52fef578edd55
TitanSnow <sweeto@live.cn>
2018-07-11 11:35:50 +0800

    refactor project configure

--- src/main.js
+++ src/main.js
@@ -1,47 +1,7 @@
 import Vue from 'vue'
-import Editor from './components/MarkdownPalettes.vue'
-import { getConfig } from './components/DefaultConfig'
-import { contentParserFactory } from './components/ContentParserFactory'
+import Editor from './module.js'
 
-Vue.config.productionTip = false
-
-class MarkdownPalettes {
-    constructor (el, config = {}) {
-        this.config = config
-        this.editor = new Vue({
-            el: el,
-            data () {
-                return {
-                    code: ''
-                }
-            },
-            render (createElement) {
-                const vm = this
-                return createElement(Editor, {
-                    props: {
-                        value: vm.code,
-                        config: this.config
-                    },
-                    on: {
-                        input (event) {
-                            vm.code = event
-                        }
-                    }
-                })
-            },
-            methods: {
-                setCode (code) {
-                    this.code = code
-                },
-                getCode () {
-                    return this.code
-                }
-            }
-        })
-    }
-    getContentParaser () {
-        return contentParserFactory(getConfig(this.config).parsers)
-    }
-}
-
-window.MarkdownPalettes = MarkdownPalettes
+new Vue({
+    el: '#editor',
+    render: h => h(Editor)
+})
---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      template: 'index.html',
+      filename: 'index.html'
+    }
+  }
+}
