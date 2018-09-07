a9b42f1b102a64ccdab051d8305a0c297bbc2516
TitanSnow <sweeto@live.cn>
2018-09-07 18:22:43 +0800

    fix #23

--- test_dist_server.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const app = express()
-app.use(require('morgan')('tiny'))
-app.use(express.static(__dirname + '/dist'))
-app.get('/', (req, rep) => void rep.redirect('/demo.html'))
-const port = 8080
-app.listen(port, () => void console.log('Listening on http://localhost:' + port))
